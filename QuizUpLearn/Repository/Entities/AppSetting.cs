a1a4b306ae3db5737d7567ef928e03020e488e48
DuongDucManh <manhmanhd0402@gmail.com>
2025-11-27 13:50:10 +0700

    Create entity to save global variable

---
+++ QuizUpLearn/Repository/Entities/AppSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Repository.Entities
+{
+    public class AppSetting
+    {
+        [Key]
+        public string Key { get; set; } = string.Empty;
+        public string? Value { get; set; }
+    }
+}
---
+++ QuizUpLearn/Repository/Migrations/20251127064747_UpdateDatabase4.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AppSettings",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AppSettings", x => x.Key);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AppSettings");
+        }
+    }
+}
