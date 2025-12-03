963ff4ce467ade2e3fe8a3be07c42d6b64e8a3c2
DuongDucManh <manhmanhd0402@gmail.com>
2025-12-03 09:58:34 +0700

    Update AIService.cs
    Not limit the weak point create same part

--- QuizUpLearn/BusinessLogic/Services/AIService.cs
+++ QuizUpLearn/BusinessLogic/Services/AIService.cs
@@ -765,8 +765,8 @@ namespace BusinessLogic.Services
                 if( answers == null || answers.Count() == 0) continue;
 
                 var userWeakPoints = await _userWeakPointService.GetByUserIdAsync(userId, null!);
-                // each user should only have 1 weak point per part + difficulty level
-                foreach (var wp in userWeakPoints.Data)
+                
+                /*foreach (var wp in userWeakPoints.Data)
                 {
                     if(wp.ToeicPart == quiz.TOEICPart
                         && wp.DifficultyLevel == quizSet.DifficultyLevel)
@@ -775,7 +775,7 @@ namespace BusinessLogic.Services
                         break;
                     }
                 }
-                if (samePartMistakeExists) continue;
+                if (samePartMistakeExists) continue;*/
 
                 await _userMistakeService.UpdateAsync(mistake.Id, new RequestUserMistakeDto
                 {
