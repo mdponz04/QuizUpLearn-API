42c195ddf0c18a450191924125775c02cc45b99a
DuongDucManh <manhmanhd0402@gmail.com>
2026-01-19 20:57:36 +0700

    Update database

--- QuizUpLearn/Repository/Entities/UserReport.cs
+++ QuizUpLearn/Repository/Entities/UserReport.cs
@@ -5,6 +5,7 @@ namespace Repository.Entities
     public class UserReport : BaseEntity
     {
         public Guid UserId { get; set; }
+        public Guid? CommentId { get; set; }
         public string Reason { get; set; } = null!;
         public virtual User? User { get; set; }
     }
